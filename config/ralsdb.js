b35ebf9c2a5e5f992234414329a98f9fd12f7215
unknown <agalya.m@binshabibgroup.ae>
2026-01-08 11:40:14 +0400

    new rals

--- config/ralsdb.js
+++ config/ralsdb.js
@@ -2,9 +2,10 @@
 const ralsdbConfig = {
 	user: 'sa',
 	password: 'gov123?',
-	server: '172.16.10.10\\sqlserver2019', // e.g., 'rals-sql-instance.xxxxxxx.region.rds.amazonaws.com'
+	//server: '172.16.10.10\\sqlserver2019', // e.g., 'rals-sql-instance.xxxxxxx.region.rds.amazonaws.com'
+	server: 'ralsnahashho.dyndns.org',
 	database: 'SAEEDSHABIBNet912626',
-   // port:14331,
+    port:14331,
 	options: {
 		encrypt: false, // Use true if connecting to Azure/AWS
 		trustServerCertificate: false // Set to true for self-signed certs
